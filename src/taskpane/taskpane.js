214d659c41631202f91487dca59e51ab7e32b863
Stage <stage@ingest.priv>
2026-02-18 09:00:35 +0100

    re fix 18/02

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -161,7 +161,7 @@ function processCell(cell) {
   const str = cell.trim();
   const numericStr = str.replace(/,/g, '.');
 
-  if (numericStr.includes('.') && /\d+\.\d+/.test(numericStr)) {
+  if (/\d+\.\d+/.test(numericStr)) {
     return parseFloat(numericStr);
   }
   return str;
@@ -188,7 +188,7 @@ function getNumericColumns(csvData) {
 
       // Se è un numero decimale (non intero) hasdecimal è true per questa colonna, altrimenti false di default
       // questo perchè nel caso in una colonna decimale ci sia solo 1 decimale anche tutti gli altri interi dovranno essere scritti coerentemente
-      if (typeof value === "number" && /\d+\.\d+/.test(value)) {
+      if (/\d+\.\d+/.test(value)) {
         hasDecimals = true;
         break; //quando trovo il primo decimale salto alla prossima colonna
       }
@@ -268,7 +268,7 @@ async function createExcelWorkbook(csvData, fileName) {
   // Genera buffer Excel
   const buffer = await workbook.xlsx.writeBuffer();
 
-  const excelFileName = fileInput.name.slice(".")[0] + ".xlsx";
+  const excelFileName = fileName.slice(".")[0] + ".xlsx";
 
   // Salva con metadata delle colonne numeriche
   workbooksToSave.push({
@@ -294,7 +294,7 @@ async function updateWorkbooksFromExcel() {
       const sheetName = workbookData.name.replace('.xlsx', ''); //per ricerca
 
       // Trova il worksheet corrispondente
-      const sheet = sheets.items.find(s => s.name === sheetName);
+      const sheet = sheets.items.find(s => s.name === sheetName.substring(0,31));
       if (!sheet) {
         console.warn(`Worksheet ${sheetName} non trovato, salto aggiornamento`);
         continue;
